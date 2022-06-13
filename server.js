2607c11f9533071a98bfa933637fda7c1d07001c
maschroder <mais1998@web.de>
2022-06-13 13:15:30 +0200

    test

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
+// Shorter Version
 let http = require('http') // use an existing module from node
-
 let host = "localhost" // 127.0.0.1
 let port = 5000
-
-let server = http.createServer(handleRequest) // create a server
-
-server.listen(port, host) // say where the server should listen
-
-function handleRequest (request, response)
-{
- console.log(request)
- console.log(response)
-}
+http.createServer((req, res) =>{
+ res.writeHead(200, {'Content-Type': 'text/html'})
+ res.write ("Hellooooooo")
+ res.end()
+}).listen(port, host)
+console.log(`Server is running on ${host}:${port}`)
