8e25953b23455482a1b685e1a80a9aa0b8ef06fd
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-06-13 13:14:40 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
+// basic version
 let http = require('http') // use an existing module from node
-
-let host = "localhost" // 127.0.0.1
-let port = 5000
 
 let server = http.createServer(handleRequest) // create a server
 
+let host = "localhost" // 127.0.0.1
+let port = 1234
+
 server.listen(port, host) // say where the server should listen
 
-function handleRequest (request, response)
+// say what the server should do, when it gets an incoming request
+function handleRequest (request, res)
 {
- console.log(request)
- console.log(response)
+    console.log(request.url)
+   
+    if(request.url === "/")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write ("We are on the main page. This is test. This is another test.")
+        res.end()
+
+    } else if (request.url === "/index.html")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write ("We are on the index.html page")
+        res.end()
+    }
+    else if  (request.url === "/longerroute/main.html")
+    {
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write ("We are on the /longerroute/main.html page")
+        res.end()
+    }
 }
+
+
+console.log(`Server is running on ${host}:${port}`)
