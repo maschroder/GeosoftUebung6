ee4ec2b065d1ac818a02419aefb42b23902a8d33
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-06-13 12:01:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-// Expanded Version
 let http = require('http') // use an existing module from node
+
 let host = "localhost" // 127.0.0.1
 let port = 5000
+
 let server = http.createServer(handleRequest) // create a server
+
 server.listen(port, host) // say where the server should listen
-// say what the server should do, when it gets an incoming request
-function handleRequest (req, res)
+
+function handleRequest (request, response)
 {
- res.writeHead(200, {'Content-Type': 'text/html'})
- res.write ("Hellooooooo")
- res.end()
+ console.log(request)
+ console.log(response)
 }
-console.log(`Server is running on ${host}:${port}`)
