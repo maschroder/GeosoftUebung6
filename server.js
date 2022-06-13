a082baee49afc1788ab5cfa2ca5b6f4ced3a287a
maschroder <90246157+maschroder@users.noreply.github.com>
2022-06-13 11:14:58 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+let http = require('http') // use an existing module from node
+let server = http.createServer(handleRequest) // create a server
+server.listen(3000) // say where the server should listen
+// say what the server should do, when it gets an incoming request
+function handleRequest (request, response)
+{
+ console.log(request)
+ console.log(response)
+}
