d8ea78f8344041bfdaaa7e8ed81c1bc7c7af5f33
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-06-20 13:50:01 +0200

    Update get.js

--- public/get.js
+++ public/get.js
@@ -1,1 +1,11 @@
 "use strict"
+
+// create map
+var map = L.map('map').setView([51.96, 7.62], 13);
+
+// Add base tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 17,
+    minZoom: 3
+}).addTo(map);
