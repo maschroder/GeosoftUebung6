3ef52b736e5371ef22d3c19eacf89c10259a2f29
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-06-20 12:04:04 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -3,6 +3,31 @@ const path = require('path')
 
 const app = express()
 const port = 3000 // unsafe ports: https://superuser.com/questions/188058/which-ports-are-considered-unsafe-by-chrome 
+const host = "localhost" //127.0.0.1
+
+app.listen(port, host)
+
+var seite1 = require('./routes/seite1');
+var seite2 = require('./routes/seite2');
+var seite3 = require('./routes/seite3');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// adding my custom routes
+// app.use('/', seite1);
+app.use('/seite1', seite1);
+app.use('/seite2', seite2);
+app.use('/seite3', seite3);
+
+
+
 
 
 
@@ -36,3 +61,13 @@ app.listen(port, () =>
 {
   console.log(`App listening at http://localhost:${port}`)
 })
+
+
+// 3 HTML Seiten erstellen, in public Ordner und über Methode aus Beispielcode aufrufen
+// .gitignore erstellen und node_modules darin ignorieren
+// Code Beispiel express_server.js -> über routen auf die verschiedenen html seiten (html to pug) zugreifen
+// Zu jeder Route in der server.js muss eine route.js und eine route.pug geschrieben werden 
+// Eine layout.pug schreiben, in der alle metadaten (leaflet...) eingebunden sind und das template (block content und block
+// foot in den einzelnen router.pug definieren)
+// in router.js dateien die funktion retrieveAllPoisFromDB nutzen
+// Minute 34: birds-router.js enthält routen. app.js starten und über diese routen, die aus bird aufrufen
