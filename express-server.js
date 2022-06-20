50a6e5be5f6f70a4758b69da47d66f8316e33563
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-06-20 11:56:22 +0200

    Update express-server.js

--- express-server.js
+++ express-server.js
@@ -5,26 +5,12 @@ const app = express()
 const port = 3000 // unsafe ports: https://superuser.com/questions/188058/which-ports-are-considered-unsafe-by-chrome 
 
 
-/* // Middleware
-var myLogger = function (req, res, next) 
-{
-  console.log('LOGGED')
-  next()
-}
-var mySecondLogger = function (req, res, next) 
-{
-  console.log('SECOND LOGGING')
-  next()
-}
-app.use(mySecondLogger)
-app.use(myLogger)
-*/
 
 // define a route
 // function taken from https://expressjs.com/de/starter/hello-world.html
 app.get('/', (req, res) => 
 {
-  //res.send('Hello World!')
+  res.send('Hello World!')
   //res.set('Content-Type', 'text/html');
   //res.sendFile(path.join(__dirname, '/public', 'example06.html'))
 })
@@ -34,7 +20,7 @@ app.get('/', (req, res) =>
 // function taken from https://expressjs.com/de/starter/static-files.html 
 //app.use(express.static('public'))
 //app.use('/static', express.static('public'))
-//app.use('/static', express.static(path.join(__dirname, 'public')))
+//app.use('/static', express.static(path.join(__dirname, 'public'))) --benutzen um leaflet einzubinden
 //console.log(__dirname) // __dirname tells you the absolute path of the directory containing the currently executing file
 //console.log(path.join(__dirname, 'public'))
 
